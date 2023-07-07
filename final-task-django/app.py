228a6f2159b677f73d60108ae40bcb289d7bb59c
Abdulkareem K.Ali <62488578+dkreem2@users.noreply.github.com>
2023-07-07 22:22:14 +0300

    Update app.py

--- final-task-django/app.py
+++ final-task-django/app.py
@@ -1,119 +1,68 @@
-import uuid
-from flask import Flask, jsonify, request
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = Flask(__name__)
+app = FastAPI()
 
-carts = {}
-orders = {}
-items = {}
 
-# Cart endpoints
+class Cart(BaseModel):
+    cart_id: int
 
-# POST /cart
-@app.route('/cart', methods=['POST'])
-def create_cart():
-    cart_id = generate_cart_id()
-    carts[cart_id] = {}
-    return jsonify({'cart_id': cart_id}), 201
+class Order(BaseModel):
+    order_id: int
 
-# GET /cart/{cart_id}
-@app.route('/cart/<cart_id>', methods=['GET'])
-def get_cart(cart_id):
-    if cart_id in carts:
-        return jsonify(carts[cart_id])
-    else:
-        return jsonify({'error': 'Cart not found'}), 404
+class Item(BaseModel):
+    item_id: int
 
-# PUT /cart/{cart_id}
-@app.route('/cart/<cart_id>', methods=['PUT'])
-def update_cart(cart_id):
-    if cart_id in carts:
-        carts[cart_id] = request.get_json()
-        return jsonify({'message': 'Cart updated', 'cart_id': cart_id})
-    else:
-        return jsonify({'error': 'Cart not found'}), 404
+@app.post("/cart")
+def create_cart(cart: Cart):
+    return {"message": "Cart created successfully"}
 
-# DELETE /cart/{cart_id}
-@app.route('/cart/<cart_id>', methods=['DELETE'])
-def delete_cart(cart_id):
-    if cart_id in carts:
-        del carts[cart_id]
-        return jsonify({'message': 'Cart deleted', 'cart_id': cart_id})
-    else:
-        return jsonify({'error': 'Cart not found'}), 404
 
-# Order endpoints
+@app.get("/cart/{cart_id}")
+def get_cart(cart_id: int):
+    return {"cart_id": cart_id}
 
-# POST /order
-@app.route('/order', methods=['POST'])
-def create_order():
-    order_id = generate_order_id()
-    orders[order_id] = {}
-    return jsonify({'order_id': order_id}), 201
 
-# GET /order/{order_id}
-@app.route('/order/<order_id>', methods=['GET'])
-def get_order(order_id):
-    if order_id in orders:
-        return jsonify(orders[order_id])
-    else:
-        return jsonify({'error': 'Order not found'}), 404
+@app.put("/cart/{cart_id}")
+def update_cart(cart_id: int, cart: Cart):
+    return {"message": "Cart updated successfully"}
 
-# PUT /order/{order_id}
-@app.route('/order/<order_id>', methods=['PUT'])
-def update_order(order_id):
-    if order_id in orders:
-        orders[order_id] = request.get_json()
-        return jsonify({'message': 'Order updated', 'order_id': order_id})
-    else:
-        return jsonify({'error': 'Order not found'}), 404
 
-# DELETE /order/{order_id}
-@app.route('/order/<order_id>', methods=['DELETE'])
-def delete_order(order_id):
-    if order_id in orders:
-        del orders[order_id]
-        return jsonify({'message': 'Order deleted', 'order_id': order_id})
-    else:
-        return jsonify({'error': 'Order not found'}), 404
+@app.delete("/cart/{cart_id}")
+def delete_cart(cart_id: int):
+    return {"message": "Cart deleted successfully"}
 
-# Item endpoints
 
-# POST /items
-@app.route('/items', methods=['POST'])
-def add_item():
-    item_id = generate_item_id()
-    items[item_id] = request.get_json()
-    return jsonify({'item_id': item_id}), 201
+@app.post("/order")
+def create_order(order: Order):
+    return {"message": "Order created successfully"}
 
-# PUT /items/{item_id}
-@app.route('/items/<item_id>', methods=['PUT'])
-def update_item(item_id):
-    if item_id in items:
-        items[item_id] = request.get_json()
-        return jsonify({'message': 'Item updated', 'item_id': item_id})
-    else:
-        return jsonify({'error': 'Item not found'}), 404
 
-# DELETE /items/{item_id}
-@app.route('/items/<item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    if item_id in items:
-        del items[item_id]
-        return jsonify({'message': 'Item deleted', 'item_id': item_id})
-    else:
-        return jsonify({'error': 'Item not found'}), 404
+@app.get("/order/{order_id}")
+def get_order(order_id: int):
+    return {"order_id": order_id}
 
-# Helper functions
 
-def generate_cart_id():
-    return str(uuid.uuid4())
+@app.put("/order/{order_id}")
+def update_order(order_id: int, order: Order):
+    return {"message": "Order updated successfully"}
 
-def generate_order_id():
-    return str(uuid.uuid4())
 
-def generate_item_id():
-    return str(uuid.uuid4())
+@app.delete("/order/{order_id}")
+def delete_order(order_id: int):
+    return {"message": "Order deleted successfully"}
 
-if __name__ == '__main__':
-    app.run()
+
+@app.post("/items")
+def add_item(item: Item):
+    return {"message": "Item added successfully"}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"message": "Item updated successfully"}
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": "Item deleted successfully"}
